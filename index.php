a4383f59db19ba33d97e4fbc41e57a3eb5bfb80d
Alejndro Lopez Ortiz <alejandro.bin@gmail.com>
2021-05-03 13:56:07 -0500

    esquemas

--- index.php
+++ index.php
@@ -3,16 +3,38 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<title>Medialoot Bulma Theme Kit</title>
-	
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+	<title>Repositorio</title>
+	<link rel="stylesheet" href="css/bulma.min.css">
 	<link rel="stylesheet" href="css/custom.css">
 	<script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
 </head>
 <body>
 	<div class="container">
-			
+		<section class="hero">
+			<h3 class="title">Respositorio</h3>
+			<h4 class="subtitle">Herramienta de publicacion de archivos</h4>
+			<hr>
+			<div class="columns">
+				<div class="column is-one-third">
+					<nav class="panel">
+						<p class="panel-heading">Carpetas</p>
+						<a class="panel-block is-active">
+							<span class="panel-icon"><i class="fa fa-folder"></i></span> No hay elementos que mostrar
+						</a>
+					</nav>
+				</div>
+				<div class="column is-two-third">
+				<nav class="panel">
+						<p class="panel-heading">Archivos</p>
+						<a class="panel-block ">
+							<span class="panel-icon"><i class="fa fa-cube"></i></span> No hay elementos que mostrar
+						</a>
+					</nav>
+				</div>
+			</div>
+			<hr>
+			</div>
+		</section>
 	</div><!--.container-->
 </body>
 </html>
