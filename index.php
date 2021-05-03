2e0c56344442eb081362ad37122f4d08631a33c8
Alejndro Lopez Ortiz <alejandro.bin@gmail.com>
2021-05-03 13:12:51 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Medialoot Bulma Theme Kit</title>
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
+</head>
+<body>
+	<div class="container">
+			
+	</div><!--.container-->
+</body>
+</html>
