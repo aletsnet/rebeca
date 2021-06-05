00249fc76ea35e39a407155b0978454b74353fc6
aletsnet <alejandro.bin@gmail.com>
2021-06-05 15:31:32 -0500

    negritas

--- index.php
+++ index.php
@@ -249,7 +249,7 @@
 
 		const load_main = async (origen = "") => {
 			let Rebeca = await load_api(origen);
-			let listCarpetas = '<p class="panel-heading">'+namecolmns+'</p>';
+			let listCarpetas = '<p class="panel-heading"><b>'+namecolmns+'</b></p>';
 			//listCarpetas += '<div class="panel-block"><p class="control has-icons-left"><input class="input is-small" type="text" placeholder="search" value="'+search+'" onkeypress="buscar_archivos(this);"><span class="icon is-small is-left"><i class="fas fa-search" aria-hidden="true"></i></span></p></div>';
 			let Tcarpetas = document.getElementById('PanelCarpetas');
 
